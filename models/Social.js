39534dbb86061d892694b724b51cc23d23419e9e
TruongHoang150602 <truonghoang150602@gmail.com>
2023-12-25 17:05:20 +0700

    done

--- models/Social.js
+++ models/Social.js
@@ -2,18 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const socialSchema = new Schema({
-    content: { type: String, require: true },
-    comments: [{ type: Schema.ObjectId, ref: "Comment" }],
-<<<<<<< HEAD
-    attachments: { type: String },
-=======
-    attachment: { type: String },
->>>>>>> 695b3c8a5e6746ebcf7403a87b00505341ac219c
-    like_list: [{ type: String }],
-    created_at: { type: Date, default: Date.now, },
-    created_by: { type: String, require: true },
-})
+  content: { type: String, require: true },
+  comments: [{ type: Schema.ObjectId, ref: "Comment" }],
+  attachment: { type: String },
+  like_list: [{ type: String }],
+  created_at: { type: Date, default: Date.now },
+  created_by: { type: String, require: true },
+});
 
-const Social = mongoose.model('Social', socialSchema);
+const Social = mongoose.model("Social", socialSchema);
 
 module.exports = Social;
